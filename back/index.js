0140825867abd331140ebb7048ea54bf9fec0207
Hyejin Eom <hjeom.dev@gmail.com>
2022-04-20 18:41:06 +0900

    Test: express 기본 실행 코드 추가

---
+++ back/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 5000 
+
+app.get('/', (req, res) => { 
+  res.send('Hello World!') 
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`)
+}) 
