835a71644cacc5d4887e3b374749612ac2e6831e
ms <altjsvnf@naver.com>
2022-04-22 20:35:38 +0900

    edit: route

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./component/main/Main";
+import MyPage from "./component/my_page/MyPage";
 import Prologue from "./component/prologue/Prologue";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Main />} />
         <Route path="/prologue" exact element={<Prologue />} />
+        <Route path="/mypage" exact element={<MyPage />} />
       </Routes>
     </Router>
   );
