afa59adf83a98272a8b6a951ceb49a96814c842e
Hyejin Eom <hjeom.dev@gmail.com>
2022-04-24 00:48:20 +0900

    Update: 영상 size 에러 수정

--- back/src/routers/talkRouter.js
+++ back/src/routers/talkRouter.js
@@ -30,11 +30,11 @@ const talkRouter = Router();
  */
 talkRouter.get("/talks/today", async (req, res, next) => {
   try {
-    const size = Number(req.query.size);
+    const size = Number(req.query.size) < 1 ? 1 : Number(req.query.size);
 
-    const talk = await talkService.getTodayTalk({ size });
+    const talks = await talkService.getTodayTalk({ size });
 
-    res.status(200).send(talk);
+    res.status(200).send(talks);
   } catch (e) {
     next(e);
   }
@@ -46,29 +46,28 @@ talkRouter.get("/talks/today", async (req, res, next) => {
  * paths:
  *  /talks/my:
  *   post:
- *     summary: "사용자 관심 주제별 추천 영상"
- *     tags: [Talk]
- *     parameters:
- *       - in: query
- *         name: size   
- *         required: true
- *         schema:
- *           type: integer
- *           minimum: 1
- *         description: 영상 개수
- *     responses:
+ *    summary: "사용자 관심 주제별 추천 영상"
+ *    tags: [Talk]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              size:
+ *                type: number
+ *    responses:
  *       "200":
  *         description: 추후 수정 예정...
  *         content:
  *           application/json:
  *             schema:
  *               $ref: '#/components/schemas/Talk'
- *     
  */
 talkRouter.post("/talks/my", /*login_required,*/ async (req, res, next) => {
     try {
-      const size = Number(req.query.size);
-      console.log(size)
+      const size = Number(req.body.size) < 1 ? 1 : Number(req.body.size);
       const user_id = req.currentUserId;
 
       const talks = await talkService.getMyTalk({ size, user_id });
