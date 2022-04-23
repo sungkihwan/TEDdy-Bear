f8cfd8076e2e76560cb2af5c123d337817075b38
sungkihwan <singahu@naver.com>
2022-04-24 00:59:12 +0900

    push test

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -9,7 +9,7 @@ const DB_URL =
   'MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요.';
 
 mongoose.connect(DB_URL);
-const db = mongoose.connection
+const db = mongoose.connection;
 
 
 db.on('connected', () =>
