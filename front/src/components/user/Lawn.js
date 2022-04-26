d4fc0533e5d794279b13e8acaf68cb4c30e70653
Manggo <watalu1209@gmail.com>
2022-04-27 04:22:57 +0900

    temp: 도저히 졸려서 못하겠어서 임시저장 ㅠ

--- front/src/components/user/Lawn.js
+++ front/src/components/user/Lawn.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as React from "react";
+import { useState, useContext, useNaviagate } from "react";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -7,7 +8,6 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import { useState } from "react";
 import Chip from "@mui/material/Chip";
 import { useNavigate } from "react-router-dom";
 import * as Api from "../../api";
@@ -16,6 +16,10 @@ import TeddyImage from "./TeddyImage";
 import UserTopics from "./UserTopics";
 import Name from "./Name";
 import Buttons from "./Buttons";
+
+import CalendarPicker from "@mui/lab/CalendarPicker";
+import MonthPicker from "@mui/x-date-pickers/MonthPicker";
+import YearPicker from "@mui/x-date-pickers/YearPicker";
 
 import isWeekend from "date-fns/isWeekend";
 import TextField from "@mui/material/TextField";
@@ -28,20 +32,36 @@ import StaticDatePicker from "@mui/lab/StaticDatePicker";
  * @returns {component} My lawn information
  */
 function Lawn() {
-  const [value, setValue] = React.useState(new Date());
+  const minDate = new Date("2021-01-01T00:00:00.000");
+  const maxDate = new Date("2099-01-01T00:00:00.000");
+  const [date, setDate] = React.useState(new Date());
+  const navigate = useNavigate();
+
+  const handleChange = async (e) => {
+    e.preventDefault();
+
+    try {
+      // "user/login" 엔드포인트로 post요청함.
+      // const res = await Api.post("user/login", {
+      //   email,
+      //   password,
+      // });
+      // console.log("로그인에 성공했씁니다.\n");
+      // 기본 페이지로 이동함.
+      navigate("/", { replace: true });
+    } catch (err) {
+      console.log("로그인에 실패하였습니다.\n", err);
+    }
+  };
+
   return (
     <Card>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <StaticDatePicker
-          orientation="landscape"
-          openTo="day"
-          value={value}
-          shouldDisableDate={isWeekend}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
+        <Grid container justifyContent="center">
+          <Grid item xs={12} md={6}>
+            <CalendarPicker date={date} onChange={handleChange} />
+          </Grid>
+        </Grid>
       </LocalizationProvider>
     </Card>
   );
