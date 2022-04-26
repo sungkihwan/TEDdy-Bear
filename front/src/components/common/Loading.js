22cd2658ce90df4cc31c67aa6f2e541a6de2b670
ms <altjsvnf@naver.com>
2022-04-26 17:58:10 +0900

    edit: loading component

--- front/src/components/common/Loading.js
+++ front/src/components/common/Loading.js
@@ -1,3 +1,13 @@
+import styled from "styled-components";
+
 export default function Loading() {
-  return <img src="/loading.gif" alt="loading" />;
+  return <Img src="/loading.gif" alt="loading" />;
 }
+
+//image style
+const Img = styled.img`
+  height: 40vh;
+  width: 25vw;
+  display: block;
+  margin: 30vh auto;
+`;
