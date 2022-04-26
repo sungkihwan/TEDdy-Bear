274d45dddaf9bacdbe0dc0ddcabfee4d0a3b2789
Manggo <watalu1209@gmail.com>
2022-04-27 08:35:27 +0900

    temp: 백엔드 소스 확인을 위해 임시저장

--- front/src/components/user/Lawn.js
+++ front/src/components/user/Lawn.js
@@ -48,7 +48,6 @@ function Lawn() {
       // });
       // console.log("로그인에 성공했씁니다.\n");
       // 기본 페이지로 이동함.
-      navigate("/", { replace: true });
     } catch (err) {
       console.log("로그인에 실패하였습니다.\n", err);
     }
