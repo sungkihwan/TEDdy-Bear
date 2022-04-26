40b0758c5f959bc0de26b4ffc4167dd97497bf36
Manggo <watalu1209@gmail.com>
2022-04-26 18:27:11 +0900

    update: npm 설정에 성공했습니다...

--- front/src/components/user/Lawn.js
+++ front/src/components/user/Lawn.js
@@ -1,11 +1,51 @@
 import styled from "styled-components";
+import * as React from "react";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import { useState } from "react";
+import Chip from "@mui/material/Chip";
+import { useNavigate } from "react-router-dom";
+import * as Api from "../../api";
+import Account from "./Account";
+import TeddyImage from "./TeddyImage";
+import UserTopics from "./UserTopics";
+import Name from "./Name";
+import Buttons from "./Buttons";
+
+import isWeekend from "date-fns/isWeekend";
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import StaticDatePicker from "@mui/lab/StaticDatePicker";
 
 /** Lawn component
  *
  * @returns {component} My lawn information
  */
-export default function Lawn() {
-  return <Page>잔디밭</Page>;
+function Lawn() {
+  const [value, setValue] = React.useState(new Date());
+  return (
+    <Grid>
+      <Page>잔디밭</Page>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <StaticDatePicker
+          orientation="landscape"
+          openTo="day"
+          value={value}
+          shouldDisableDate={isWeekend}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+    </Grid>
+  );
 }
 
 //page style
@@ -18,3 +58,5 @@ const Page = styled.div`
   align-items: center;
   position: relative;
 `;
+
+export default Lawn;
