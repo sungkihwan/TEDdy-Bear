3ddf0d088bc4ccbafce9d2e7949b029090a4a235
ys <lhyahoo0119@gmail.com>
2022-04-26 11:49:19 +0900

    bear schema 수정

--- back/src/db/schemas/bear.js
+++ back/src/db/schemas/bear.js
@@ -3,6 +3,9 @@ import { Schema, model } from 'mongoose';
 // 곰 캐릭터 mvp
 // 뭐가 필요할까?
 // 곰 이름, 곰 레벨, 경험치
+//level, cotton, height
+// cotton ->  level -> height
+// 영상을 보면 cotton
 const bearSchema = new Schema({
   userId: {
     type: String,
@@ -13,10 +16,6 @@ const bearSchema = new Schema({
     required: true,
   },
   level: {
-    type: Number,
-    required: true,
-  },
-  exp: {
     type: Number,
     required: true,
   },
