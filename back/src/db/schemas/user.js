5ce6cc924bd280146e7c21b87266663455d0629a
Hyejin Eom <hjeom.dev@gmail.com>
2022-04-27 15:00:54 +0900

    Update: infoProvider에 설정되지 않은 value가 설정될 때 반환되는 에러메세지를 수정

--- back/src/db/schemas/user.js
+++ back/src/db/schemas/user.js
@@ -33,7 +33,7 @@ const UserSchema = new Schema(
       type: String,
       enum: {
         values: ["User", "Google"],
-        message: "{VALUE} is not supported",
+        message: "{VALUE} 로 로그인하기는 아직 지원되지 않습니다.",
       },
     },
   },
