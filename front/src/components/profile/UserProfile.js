3c3c416b91fefe48312d487507e985f7926e6b55
ms <altjsvnf@naver.com>
2022-04-29 22:48:31 +0900

    edit: edit user data page

--- front/src/components/profile/UserProfile.js
+++ front/src/components/profile/UserProfile.js
@@ -4,18 +4,29 @@ import { ProfileCard, ProfileImg, ProfileText, UserInfo } from "./styles/Style";
 
 function UserProfile({ user, isEditable }) {
   const navigate = useNavigate();
-
+  const topicDict = {
+    technology: "기술",
+    science: "과학",
+    culture: "문화",
+    globalissues: "글로벌이슈",
+    society: "사회",
+    design: "디자인",
+    socialchange: "사회변화",
+    business: "비즈니스",
+    animation: "애니메이션",
+    health: "건강",
+  };
   return (
     <ProfileCard>
       <ProfileImg src="/mybear.png" alt="bear" />
       <UserInfo>
         <ProfileText>{user.name}</ProfileText>
         <ProfileText>{user.email}</ProfileText>
-        <ProfileText style={{ fontSize: 15 }}>{user.description}</ProfileText>
+        <ProfileText>{user.description}</ProfileText>
         <ProfileText>
           {user.myTopics &&
             user.myTopics.map((topic, index) => (
-              <span key={index}>{topic} </span>
+              <span key={index}>{topicDict[topic]} </span>
             ))}
         </ProfileText>
         {user.age && <ProfileText>{user.age} 세</ProfileText>}
