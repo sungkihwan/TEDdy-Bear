c62071c321c89a9a016fa1d15f7fe7c5d74d63e3
sungkihwan <singahu@naver.com>
2022-05-02 23:35:23 +0900

    이메일전송, ttl 로직 수정

--- back/src/db/models/Ttl.js
+++ back/src/db/models/Ttl.js
@@ -2,11 +2,11 @@ import { TtlModel } from '../schemas/ttl';
 
 class Ttl {
   static async create({ newItem }) {
-    return await TtlModel.create({newItem});
+    return await TtlModel.create(newItem);
   }
 
-  static async findById({ code }) {
-    return TtlModel.findOne({ code });
+  static async find({ code }) {
+    return TtlModel.find({ code });
   }
 }
 
