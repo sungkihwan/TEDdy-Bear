12e1462e18b2ccfc20fbb05f29fd3680c16d3179
ys <lhyahoo0119@gmail.com>
2022-05-03 21:35:24 +0900

    시청기록 수정

--- back/src/db/models/Talk.js
+++ back/src/db/models/Talk.js
@@ -1,8 +1,8 @@
-import { TalkModel } from "../schemas/talk";
+import { TalkModel } from '../schemas/talk';
 
 class Talk {
   static findOneById({ id, resultType }) {
-    if (resultType === "document") { 
+    if (resultType === 'document') {
       return TalkModel.findOne({ id: id });
     } //else if(resultType === "POJO") {
     return TalkModel.findOne({ id: id }).lean();
@@ -24,6 +24,9 @@ class Talk {
     return TalkModel.findOneAndUpdate({ _id: id }, toUpdate, {
       returnOriginal: false,
     });
+  }
+  static findByOid({ _id }) {
+    return TalkModel.findOne({ _id: _id });
   }
 }
 
--- back/src/db/models/ViewHistory.js
+++ back/src/db/models/ViewHistory.js
@@ -4,14 +4,6 @@
  * 시청 기록 생성, 조회, 수정
  */
 
-// 언제 시청 기록이 생성되나?
-// 동영상 링크를 클릭했을 때!
-// 기록하는 정보는 user_id, talkId, url, title
-// 일단 클릭할 때마다 view history를 남겨놓고 나중에 viewDate가 같은 것끼리 묶어서 날짜별로 관리를 한다.
-// 그러면?
-// 생성, 조회만 필요
-// 수정, 삭제는 필요없음
-
 import { ViewHistoryModel } from '../schemas/viewHistory';
 
 class ViewHistory {
@@ -27,12 +19,16 @@ class ViewHistory {
 
   // findManyByUserId() : 모든 시청기록 가져오기
   static async findManyByUserId({ user_id }) {
-    return ViewHistoryModel.find({ user_id });
+    return ViewHistoryModel.find({ user_id })
+      .populate({ path: 'talkId' })
+      .lean();
   }
 
   // createAt으로 4달 전까지의 데이터 찾아서 보내기
   static async findManyByCreatedAt({ user_id, size }) {
-    const userViewHistorylist = ViewHistoryModel.find({ user_id });
+    const userViewHistorylist = ViewHistoryModel.find({ user_id })
+      .populate({ path: 'talkId' })
+      .lean();
     const dateBefore = new Date();
     const now = new Date();
     dateBefore.setMonth(now.getMonth() - size);
@@ -58,7 +54,7 @@ class ViewHistory {
       },
       {
         $group: {
-          _id: '$url',
+          _id: '$talkId',
         },
       },
     ]);
