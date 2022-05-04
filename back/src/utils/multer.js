0061455f5adbc1d1fa70215b55371a601bb68325
BoSeon Park <bedurgi999@gmail.com>
2022-05-04 16:11:07 +0900

    merge

--- back/src/utils/multer.js
+++ back/src/utils/multer.js
@@ -1,22 +1,22 @@
-import * as  multer from 'multer';
-import MulterGoogleCloudStorage from 'multer-google-storage';
+import * as multer from "multer";
+import MulterGoogleCloudStorage from "multer-google-storage";
 
 const bucket = process.env.GCS_BUCKET;
 const projectId = process.env.GCLOUD_PROJECT;
 const keyFilename = process.env.GCS_KEYFILE;
 
 const uploadHandler = multer({
-    storage: MulterGoogleCloudStorage.storageEngine({
-        autoRetry: true,
-        maxRetries: 3,
-        bucket: bucket,
-        projectId: projectId,
-        keyFilename: keyFilename,
-        filename: (req, file, cb) => {
-            cb(null, `/images/${Date.now()}_${filename}`);
-        }
-    }),
-    limits: { fileSize: 2 * 1024 * 1024 }
+  storage: MulterGoogleCloudStorage.storageEngine({
+    autoRetry: true,
+    maxRetries: 3,
+    bucket: bucket,
+    projectId: projectId,
+    keyFilename: keyFilename,
+    filename: (req, file, cb) => {
+      cb(null, `/images/${Date.now()}_${filename}`);
+    },
+  }),
+  limits: { fileSize: 2 * 1024 * 1024 },
 });
 
-export { uploadHandler }
+export { uploadHandler };
