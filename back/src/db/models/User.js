0238f5f2c075d5fab3de4595805a8332908b3b80
ys <lhyahoo0119@gmail.com>
2022-05-04 15:27:14 +0900

    edit: User 파일 수정

--- back/src/db/models/User.js
+++ back/src/db/models/User.js
@@ -16,8 +16,8 @@ class User {
     return user;
   }
 
-  static async findById({ user_id }) {
-    const user = await UserModel.findOne({ id: user_id });
+  static async findById({ userId, user_id }) {
+    const user = await UserModel.findOne({ id: userId ?? user_id });
     return user;
   }
 
